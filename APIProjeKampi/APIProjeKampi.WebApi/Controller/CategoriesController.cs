daffc2332da884e0291ae24cbb65f084c6bb35e2
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-18 18:01:56 +0300

    Added CategoryController

--- APIProjeKampi/APIProjeKampi.WebApi/Controller/CategoriesController.cs
+++ APIProjeKampi/APIProjeKampi.WebApi/Controller/CategoriesController.cs
@@ -16,6 +16,13 @@ namespace APIProjeKampi.WebApi.Controller
             _context = context;
         }
 
+        [HttpGet]
+        public IActionResult CategoryResult()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
         [HttpPost]
         public IActionResult AddCategory(Category category)
         {
@@ -23,5 +30,31 @@ namespace APIProjeKampi.WebApi.Controller
             _context.SaveChanges();
             return Ok("Kategori Başarıyla Eklendi.");
         }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok($"{id} nolu kategori silindi.");
+        }
+
+        // WARNING : We cannot use the same attribute in the same controller.
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            var value = _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Başarıyla Güncellendi.");
+        }
+
     }
 }
