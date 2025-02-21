5624c8841a7caca410223eff8695625b4b340da0
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-02-21 03:16:59 +0300

    Changed variable type (End of 10th video)

--- APIProjeKampi/APIProjeKampi.WebApi/Dtos/ContactDtos/CreateContactDto.cs
+++ APIProjeKampi/APIProjeKampi.WebApi/Dtos/ContactDtos/CreateContactDto.cs
@@ -4,7 +4,7 @@
     {
         public string MapLocation { get; set; }
         public string Address { get; set; }
-        public int Phone { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
         public string OpeningHours { get; set; }
     }
