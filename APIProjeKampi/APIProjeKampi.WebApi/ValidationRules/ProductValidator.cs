72e7826d18930c83bcbc32efc5cd612a60ccf1ea
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-03-08 22:44:48 +0300

    Create ProductValidator.cs

---
+++ APIProjeKampi/APIProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using APIProjeKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace APIProjeKampi.WebApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Ürün adı girilmesi zorunludur");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter girilmelidir.");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En fazla 50 karakter girilebilir");
+
+        }
+
+    }
+}
