c31cdb8af4899ef92ee33f320659d3707b2368f3
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-03-09 01:34:58 +0300

    Update Product Validator (End of 14th video)

--- APIProjeKampi/APIProjeKampi.WebApi/ValidationRules/ProductValidator.cs
+++ APIProjeKampi/APIProjeKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -12,6 +12,13 @@ namespace APIProjeKampi.WebApi.ValidationRules
             RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("En az 2 karakter girilmelidir.");
             RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("En fazla 50 karakter girilebilir");
 
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı girilmesi zorunludur.")
+                .LessThan(0).WithMessage("Ürün fiyatı negatif olamaz")
+                .GreaterThan(10000).WithMessage("Ürün fiyatı 10.000'den yüksek olamaz");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Ürüne ait açıklama girmek zorunludur.")
+                .MinimumLength(3).WithMessage("Ürüne ait açıklama isminden kısa olamaz");
+
         }
 
     }
