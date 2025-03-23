bb84fd85bf1469d1cb58b41b4453c82a13f1c129
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-03-23 23:54:49 +0300

    Created ResultProductDto

---
+++ APIProjeKampi/APIProjeKampi.WebApi/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,17 @@
+﻿using APIProjeKampi.WebApi.Entities;
+
+namespace APIProjeKampi.WebApi.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public int CategoryId { get; set; }
+        public Category CategoryName { get; set; }
+
+
+    }
+}
