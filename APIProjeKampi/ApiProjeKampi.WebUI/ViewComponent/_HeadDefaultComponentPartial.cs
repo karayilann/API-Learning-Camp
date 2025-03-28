3f9b125c6f470abe37933ec169bed34215e2e2c6
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-03-28 23:32:50 +0300

    Added Components (End of 19th Video)

---
+++ APIProjeKampi/ApiProjeKampi.WebUI/ViewComponent/_HeadDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiProjeKampi.WebUI.ViewComponent
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class _HeadDefaultComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    
+    }
+}
