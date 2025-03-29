f5d3389bc1d850ea24bb3ecf77400c9b50cd49b3
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-03-30 02:13:04 +0300

    Deleted All Web UI files

--- APIProjeKampi/ApiProjeKampi.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ApiProjeKampi.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
