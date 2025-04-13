a963c26a733ff86543fcc49b092b524bef1b33dd
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-04-13 15:56:23 +0300

    Continued to Create Partial Components (End Of 21th Video)

---
+++ APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/_AboutDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIProjeKampi.WebUI.ViewComponents
+{
+    public class _AboutDefaultComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIProjeKampi.WebUI.ViewComponents
+{
+    public class _FeatureDefaultComponentPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
