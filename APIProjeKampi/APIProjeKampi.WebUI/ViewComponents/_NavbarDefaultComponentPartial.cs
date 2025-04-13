b7dcc079e75d8d1ba5066b41e7e6034617d0dac4
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-04-13 14:51:38 +0300

    Created Navbar Component

---
+++ APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/_NavbarDefaultComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIProjeKampi.WebUI.ViewComponents
+{
+    public class _NavbarDefaultComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
