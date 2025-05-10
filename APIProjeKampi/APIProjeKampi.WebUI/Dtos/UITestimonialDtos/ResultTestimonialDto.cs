66d6b9d4199cd1b6753e9e4c15f57bac6db2721f
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-05-11 01:07:27 +0300

    Created Testimonials Section & Update ViewImport.cshtml

---
+++ APIProjeKampi/APIProjeKampi.WebUI/Dtos/UITestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace APIProjeKampi.WebUI.Dtos.UITestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+        public string NameSurname { get; set; }
+        public string Comment { get; set; }
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        
+    }
+}
--- APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
+++ APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/_TestimonialDefaultComponentPartial.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIProjeKampi.WebUI.Dtos.UITestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace APIProjeKampi.WebUI.ViewComponents
 {
@@ -11,12 +13,20 @@ namespace APIProjeKampi.WebUI.ViewComponents
             _clientFactory = clientFactory;
         }
 
-        private async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _clientFactory.CreateClient();
-            client.GetAsync("https://localhost:7086/api/Testimonials/");
-        }
+            var message = await client.GetAsync("https://localhost:7086/api/Testimonials/");
 
+            if (message.IsSuccessStatusCode)
+            {
+                var readAsStringAsync = await message.Content.ReadAsStringAsync();
+                var deserializeObject = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(readAsStringAsync);
+                return View(deserializeObject);
+            }
+
+            return View();
+        }
 
     }
 }
