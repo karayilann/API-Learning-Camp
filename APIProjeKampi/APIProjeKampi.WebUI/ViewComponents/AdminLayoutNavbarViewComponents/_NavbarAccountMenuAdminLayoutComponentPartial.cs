89e3e17ed8aeb0017655d0503b004d1e1fb642df
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2025-06-11 23:10:30 +0300

    Completed Navbar

---
+++ APIProjeKampi/APIProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarAccountMenuAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarAccountMenuAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
